403a8eb2d845584696169719c2aa5f9ba1c46466
Ayush <12shadowrider@gmail.com>
2023-06-30 13:57:40 +0530

    first commit

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex justify-between p-4 font-hankenRegular'>
+        <h1>Dashboard</h1>
+        <h1>Welcome back</h1>
+    </div>
+  )
+}
+
+export default Header
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,32 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors:{
+        "pale-blue":"hsl(221,100%,96%)",
+        "slate-blue":{
+          "dark":"hsl(241,81%,54%)",
+          "light":"hsl(252,100%,67%)"
+        },
+        "violet-blue":"hsl(256,72%,46%,1)",
+        "dark-gray-blue":"hsl(224,30%,27%)",
+        "memory":"hsl(39, 100%, 56%)",
+        "verbal":"hsl(166, 100%, 37%)",
+        "visual":"hsl(234, 85%, 45%)",
+        "light-red":{
+          default:"hsl(0,100%,67%)",
+          light:"hsla(0,100%,67%,.1)",
+          memory:"hsla(39, 100%, 56%,.1)",
+          visual:"hsla(234, 85%, 45%,.1)",
+          verbal:"hsla(166, 100%, 37%,.1)"
+        }
+      },
+      fontFamily: {
+        hanken: ["Hanken Grotesk", "sans-serif"],
+      },
+      fontWeight: {
+        hankenRegular: 500,
+        hankenBold: 700,
+        hankenExtraBold: 700,
       },
     },
   },
